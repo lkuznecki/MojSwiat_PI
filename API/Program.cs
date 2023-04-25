3583e32e27571a64392a0b9fb2b293eb481376c8
lkuznecki <lukasz.kuznecki@gmail.com>
2023-04-25 18:09:57 +0200

    End of stage 4

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>)); // list generic repository as service
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -25,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
