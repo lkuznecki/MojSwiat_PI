5893c48d7d672f956cf1cd90c0a9387702bf2931
lkuznecki <lukasz.kuznecki@gmail.com>
2023-05-02 07:22:59 +0200

    Angular client basket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Mój świat';
+  title = 'Skinet';
 
-  constructor(){}
+  constructor(private basketService: BasketService) {}
 
-  ngOnInit(): void{
-
+  ngOnInit(): void {
+    this.loadBasket();
   }
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) this.basketService.getBasket(basketId);
+  }
+
 }
 
 
