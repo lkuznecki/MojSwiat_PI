8515baa93e32676fcf92536373cef435910d48d6
lkuznecki <lukasz.kuznecki@gmail.com>
2023-05-02 11:53:39 +0200

    API-Identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 using API.Extensions;
 using API.Middleware;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationservices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 
 var app = builder.Build();
@@ -26,16 +30,23 @@ app.UseStaticFiles();
 app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope(); //Dispose method will be called once we are done using this
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
 catch (Exception ex)
 {
